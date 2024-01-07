fb38eb3c3a8e88e7c36184840b1d523721da3b62
lord-charles <mwanikicharles226@gmail.com>
2024-01-08 00:35:34 +0300

    2

--- app/components/dashboard/Table.js
+++ app/components/dashboard/Table.js
@@ -42,7 +42,7 @@ const DashboardTable = () => {
     <Card>
       <div>
         <h2 className="text-black text-center p-4 font-semibold text-[19px]">
-          Current & previous occupants in F6
+          F6 Occupancy History
         </h2>
       </div>
       <TableContainer>
